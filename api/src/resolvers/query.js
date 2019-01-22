ce2505efaa21dc968821f769109eaa6c2989c72f
Wim Klerkx <wim@videodock.com>
2019-01-22 16:04:37 +0100

    Add fulltext index search query

--- api/src/resolvers/query.js
+++ api/src/resolvers/query.js
@@ -43,5 +43,51 @@ export const queryResolvers = {
           return toReturn;
         })
     },
+    fulltextSearchMetadata(object, params, context, resolveInfo){
+      // determine types to include in search: types passed with query parameters, or if left empty; all metadataInterfaced types
+      const searchIncludedTypes = (params.onTypes === undefined || params.onTypes.length == 0) ? resolveInfo.schema._typeMap.MetadataInterfacedType._values : params.onTypes;
+      // // generate query clause for the types included in the query
+      // let typeClause = "(";
+      // typeClause += searchIncludedTypes.map(
+      //   type => {
+      //     const typeName = (type.hasOwnProperty('name')) ? type.name : type;
+      //     return "`node`:`" + typeName + "` OR ";
+      //   }).join('');
+      // typeClause = typeClause.substring(0,typeClause.length-4) + ")";
+      //
+      // // determine metadata fields to evaluate in the search query: fields passed with query parameters, or if left empty; all SearchableMetadataFields
+      // const searchIncludedFields = (params.onFields === undefined || params.onFields.length == 0) ? resolveInfo.schema._typeMap.SearchableMetadataFields._values : params.onFields;
+      // // generate query clause for the metadata fields to be searched
+      // let fieldClause = "(";
+      // fieldClause += searchIncludedFields.map(
+      //   field => {
+      //     const fieldName = (field.hasOwnProperty('name')) ? field.name : field;
+      //     return "toLower(`node`.`" + fieldName + "`) CONTAINS toLower($substring) OR ";
+      //   }).join('');
+      // fieldClause = fieldClause.substring(0,fieldClause.length-4) + ")";
+
+      // const searchQuery = "MATCH (`node`) WHERE " + typeClause + " AND " + fieldClause + " RETURN `node` as `result` SKIP $offset LIMIT $first";
+      const searchQuery = "CALL apoc.index.search(\"metadata\", \"" + params.substring + "~\") YIELD `node`, `weight` RETURN `node`, `weight` ORDER BY `weight` DESC SKIP $offset LIMIT $first";
+      console.log(searchQuery);
+      let session = driver.session();
+      return session.run(searchQuery, params)
+        .then( result => {
+          const toReturn = result.records.map(
+            record => {
+              //console.log(record.get("node"));
+              //console.log(record.get("weight"));
+              const object = record.get("node").properties;
+              object.weight = record.get("weight");
+              const labels = record.get("node").labels;
+              if (labels instanceof Array && labels.length > 0){
+                object._schemaType = labels.shift();
+                object._additionalSchemaType = labels;
+              }
+              console.log(object);
+              return object;
+            })
+          return toReturn;
+        })
+    }
   }
 }
