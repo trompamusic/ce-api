38e6b151104e9cad8e8d4d3d65d36250249d95a9
Wim Klerkx <wim@videodock.com>
2019-01-24 14:49:53 +0100

    refactor

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -4,65 +4,40 @@ import { retrieveNodeData } from "../resolvers"
 
 export const mutationResolvers = {
   Mutation: {
-    AddMusicCompositionAuthor (object, params, ctx, resolveInfo) {
-      let session = driver.session();
-      let query = "MATCH (`musicComposition_from`:`MusicComposition` {identifier: $from.identifier})" +
-        " MATCH (`legalPerson_to`: `" + params.to.type + "` {identifier: $to.identifier})" +
-        " CREATE (`musicComposition_from`)-[`author_relation`:`AUTHOR`]->(`legalPerson_to`)" +
-        " RETURN { from: `musicComposition_from` ,to: `legalPerson_to` } AS `_AddMusicCompositionAuthorPayload`;"
-
-      let promise = session.run(query, params)
-        .then( result => {
-          let rt = result.records.map(record => {
-            let data = record.get("_AddMusicCompositionAuthorPayload");
-            return {from:data.from.properties, to:data.to.properties};
-          });
-          return rt[0];
-        })
-        .catch(function (error) {console.log(error);});
-
-      return promise;
-    },
-    AddCreativeWorkInterfaceExampleOfWork (object, params, ctx, resolveInfo) {
-      let session = driver.session();
-      let query = "MATCH (`creativeWorkInterface_from`:`" + params.from.type + "` {identifier: $from.identifier})" +
-        " MATCH (`creativeWorkInterfaced_to`: `" + params.to.type + "` {identifier: $to.identifier})" +
-        " CREATE (`creativeWorkInterface_from`)-[`exampleOfWork_relation`:`EXAMPLE_OF_WORK`]->(`creativeWorkInterfaced_to`)" +
-        " RETURN { from: `creativeWorkInterface_from` ,to: `creativeWorkInterfaced_to` } AS `_AddCreativeWorkInterfaceExampleOfWorkPayload`;"
-
-      let promise = session.run(query, params)
-        .then( result => {
-          let rt = result.records.map(record => {
-            let data = record.get("_AddCreativeWorkInterfaceExampleOfWorkPayload");
-            return {from:data.from.properties, to:data.to.properties};
-          });
-          return rt[0];
-        })
-        .catch(function (error) {console.log(error);});
-
-      return promise;
-    },
     AddCreativeWorkInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      let session = driver.session();
-      let query = "MATCH (`creativeWorkInterface_from`:`" + params.from.type + "` {identifier: $from.identifier})" +
-        " MATCH (`legalPerson_to`: `" + params.to.type + "` {identifier: $to.identifier})" +
-        " CREATE (`creativeWorkInterface_from`)-[`exampleOfWork_relation`:`" + snakeCase(params.field).toUpperCase() + "`]->(`legalPerson_to`)" +
-        " RETURN { from: `creativeWorkInterface_from` ,to: `legalPerson_to` } AS `_AddCreativeWorkInterfaceLegalPersonPayload`;"
-
-      let promise = session.run(query, params)
-        .then( result => {
-          let rt = result.records.map(record => {
-            const payload = record.get("_AddCreativeWorkInterfaceLegalPersonPayload");
-            return {
-              from:retrieveNodeData(payload.from),
-              to:retrieveNodeData(payload.to)
-            };
-          });
-          return rt[0];
-        })
-        .catch(function (error) {console.log(error);});
-
-      return promise;
-    }
+      return runAdd(params);
+    },
+    AddCreativeWorkInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
+      return runAdd(params);
+    },
   }
+}
+
+const runAdd = function (params) {
+  return runAddQuery(generateAddQuery(params));
+}
+
+const generateAddQuery = function (params) {
+  return "MATCH (`node_from`:`" + params.from.type + "` {identifier: \"" + params.from.identifier + "\"})" +
+    " MATCH (`node_to`: `" + params.to.type + "` {identifier: \"" + params.to.identifier + "\"})" +
+    " CREATE (`node_from`)-[`relation`:`" + snakeCase(params.field).toUpperCase() + "`]->(`node_to`)" +
+    " RETURN { from: `node_from` ,to: `node_to` } AS `_AddedPayload`;";
+}
+
+const runAddQuery = function (query) {
+  let session = driver.session();
+  let promise = session.run(query)
+    .then( result => {
+      let rt = result.records.map(record => {
+        const payload = record.get("_AddedPayload");
+        return {
+          from:retrieveNodeData(payload.from),
+          to:retrieveNodeData(payload.to)
+        };
+      });
+      return rt[0];
+    })
+    .catch(function (error) {console.log(error);});
+
+  return promise;
 }
