ba1dae9aeb1928b010e260ed70d7ed88050b809b
Wim Klerkx <wim@videodock.com>
2019-01-28 16:32:15 +0100

    debug DELETE function

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -5,146 +5,157 @@ import { retrieveNodeData } from "../resolvers"
 export const mutationResolvers = {
   Mutation: {
     AddThingInterfaceThingInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveThingInterfaceThingInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddThingInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveThingInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddThingInterfacePotentialAction (object, params, ctx, resolveInfo) {
       params.field = 'potentialAction';
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveThingInterfacePotentialAction (object, params, ctx, resolveInfo) {
       params.field = 'potentialAction';
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddCreativeWorkInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveCreativeWorkInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddOrganizationInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveOrganizationInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddCreativeWorkInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveCreativeWorkInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddMediaObjectInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveMediaObjectInterfaceCreativeWorkInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddActionInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params);
     },
     RemoveActionInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params);
     },
     AddDigitalDocumentPermissionGrantee (object, params, ctx, resolveInfo) {
       params.from.type = 'DigitalDocumentPermission';
       params.field = 'grantee';
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveDigitalDocumentPermissionGrantee (object, params, ctx, resolveInfo) {
       params.from.type = 'DigitalDocumentPermission';
       params.field = 'grantee';
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddEventComposer (object, params, ctx, resolveInfo) {
       params.from.type = 'Event';
       params.field = 'composer';
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveEventComposer (object, params, ctx, resolveInfo) {
       params.from.type = 'Event';
       params.field = 'composer';
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddEventPerformer (object, params, ctx, resolveInfo) {
       params.from.type = 'Event';
       params.field = 'performer';
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveEventPerformer (object, params, ctx, resolveInfo) {
       params.from.type = 'Event';
       params.field = 'performer';
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddVideoObjectMusicBy (object, params, ctx, resolveInfo) {
       params.from.type = 'VideoObject';
       params.field = 'musicBy';
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveVideoObjectMusicBy (object, params, ctx, resolveInfo) {
       params.from.type = 'VideoObject';
       params.field = 'musicBy';
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddProvenanceEntityInterfaceWasAttributedTo (object, params, ctx, resolveInfo) {
       params.field = 'wasAttributedTo';
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveProvenanceEntityInterfaceWasAttributedTo (object, params, ctx, resolveInfo) {
       params.field = 'wasAttributedTo';
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddProvenanceAgentInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveProvenanceAgentInterfaceLegalPerson (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddActionInterfaceThingInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveActionInterfaceThingInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddProvenanceEntityInterfaceThingInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveProvenanceEntityInterfaceThingInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddProvenanceEntityInterfaceWasGeneratedBy (object, params, ctx, resolveInfo) {
       params.field = 'wasGeneratedBy';
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveProvenanceEntityInterfaceWasGeneratedBy (object, params, ctx, resolveInfo) {
       params.field = 'wasGeneratedBy';
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
     AddProvenanceActivityInterfaceActionInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'CREATE');
+      return runAdd(params, 'CREATE');
     },
     RemoveProvenanceActivityInterfaceActionInterface (object, params, ctx, resolveInfo) {
-      return run(params, 'DELETE');
+      return runRemove(params, 'DELETE');
     },
   }
 }
 
-const run = function (params, action) {
-  return runQuery(generateQuery(params, action));
+const runAdd = function (params) {
+  return runQuery(generateAddQuery(params));
 }
 
-const generateQuery = function (params, action) {
+const runRemove = function (params) {
+  return runQuery(generateRemoveQuery(params));
+}
+
+const generateAddQuery = function (params) {
   return "MATCH (`node_from`:`" + params.from.type + "` {identifier: \"" + params.from.identifier + "\"})" +
     " MATCH (`node_to`: `" + params.to.type + "` {identifier: \"" + params.to.identifier + "\"})" +
-    " " + action + " (`node_from`)-[`relation`:`" + snakeCase(params.field).toUpperCase() + "`]->(`node_to`)" +
+    " CREATE (`node_from`)-[`relation`:`" + snakeCase(params.field).toUpperCase() + "`]->(`node_to`)" +
+    " RETURN { from: `node_from` ,to: `node_to` } AS `_payload`;";
+}
+
+const generateRemoveQuery = function (params) {
+  return "MATCH (`node_from`:`" + params.from.type + "` {`identifier`: \"" + params.from.identifier + "\"})" +
+    " -[`relation`:" + snakeCase(params.field).toUpperCase() + "]-> (`node_to`: `" + params.to.type + "` {`identifier`: \"" + params.to.identifier + "\"})" +
+    " DELETE `relation`" +
     " RETURN { from: `node_from` ,to: `node_to` } AS `_payload`;";
 }
 
@@ -153,7 +164,7 @@ const runQuery = function (query) {
   let promise = session.run(query)
     .then( result => {
       let rt = result.records.map(record => {
-        const payload = record.get("_AddedPayload");
+        const payload = record.get("_payload");
         return {
           from:retrieveNodeData(payload.from),
           to:retrieveNodeData(payload.to)
