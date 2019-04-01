106902b74f58db2c589ee74620785a16836cda5f
Christiaan Scheermeijer <christiaan@videodock.com>
2019-04-01 11:42:40 +0200

    feat(project): make code style project wide

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,25 +1,25 @@
 import { execute as exec, makePromise } from 'apollo-link'
-import { HttpLink } from 'apollo-link-http';
-import fetch from 'node-fetch';
+import { HttpLink } from 'apollo-link-http'
+import fetch from 'node-fetch'
 
-const link = new HttpLink({ uri: 'http://localhost:4000/graphql', fetch });
+const link = new HttpLink({ uri: 'http://localhost:4000/graphql', fetch })
 
 export const execute = (query, variables) => {
   return makePromise(exec(link, { query, variables }))
-};
+}
 
 expect.extend({
-  toContainData(received, key) {
+  toContainData (received, key) {
     if (received && received.errors) {
-      const message = received.errors.map(error => error.message).join('\n');
+      const message = received.errors.map(error => error.message).join('\n')
 
-      return { message: () => `expected response to not contain errors: \n\n${message}`, pass: false };
+      return { message: () => `expected response to not contain errors: \n\n${message}`, pass: false }
     }
 
     if (received && received.data && received.data[key] !== null) {
-      return { pass: true };
+      return { pass: true }
     }
 
-    return { message: () => `expected response to contain the data ${key}`, pass: false };
-  },
-});
+    return { message: () => `expected response to contain the data ${key}`, pass: false }
+  }
+})
--- tests/personBySubstring.test.js
+++ tests/personBySubstring.test.js
@@ -1,5 +1,5 @@
-import gql from 'graphql-tag';
-import { execute } from './helpers';
+import gql from 'graphql-tag'
+import { execute } from './helpers'
 
 const PERSON_BY_SUBSTRING_QUERY = gql`
     query($substring: String!) {
@@ -7,22 +7,22 @@ const PERSON_BY_SUBSTRING_QUERY = gql`
             name
         }
     }
-`;
+`
 
 describe('query personBySubstring', () => {
   it('should return results when using an empty substring', () => {
     return execute(PERSON_BY_SUBSTRING_QUERY, { substring: '' })
       .then(response => {
-        expect(response).toContainData('personBySubstring');
-        expect(response.data.personBySubstring.length).toEqual(7);
-      });
-  });
+        expect(response).toContainData('personBySubstring')
+        expect(response.data.personBySubstring.length).toEqual(7)
+      })
+  })
 
   it('should return filtered results when searching on a substring', () => {
     return execute(PERSON_BY_SUBSTRING_QUERY, { substring: 'Ma' })
       .then(response => {
-        expect(response).toContainData('personBySubstring');
-        expect(response.data.personBySubstring.length).toEqual(3);
-      });
-  });
-});
+        expect(response).toContainData('personBySubstring')
+        expect(response.data.personBySubstring.length).toEqual(3)
+      })
+  })
+})
