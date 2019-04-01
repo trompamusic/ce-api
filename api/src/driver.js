d54c032f12be704e98026f4d33a08ec70eb0dbe8
Christiaan Scheermeijer <christiaan@videodock.com>
2019-04-01 12:24:12 +0200

    chore(api): update default neo4j password

--- api/src/driver.js
+++ api/src/driver.js
@@ -9,6 +9,6 @@ export const driver = neo4j.driver(
   process.env.NEO4J_URI || 'bolt://localhost:7687',
   neo4j.auth.basic(
     process.env.NEO4J_USER || 'neo4j',
-    process.env.NEO4J_PASSWORD || 'neo4j'
+    process.env.NEO4J_PASSWORD || 'letmein'
   )
 )
