b2f287e39b0705f87038be9c170a24bf056f6712
Wim Klerkx <wim@videodock.com>
2019-04-03 16:55:31 +0200

    allow graphql path

--- src/index.js
+++ src/index.js
@@ -3,10 +3,33 @@ import { schema } from './schema'
 import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
 import validator from 'validator'
+import bodyParser from 'body-parser'
 import { debug as Debug } from 'debug'
 export const debug = Debug('ce-api-debug')
 export const info = Debug('ce-api-info')
 export const warning = Debug('ce-api-warning')
+
+const app = express()
+app.use(bodyParser.json());
+
+const restRequest = function (req, res, next) {
+  const identifier = req.params.identifier
+  // allow /graphql path to be resolved
+  if (identifier.toLowerCase() === 'graphql') {
+    next()
+    return
+  }
+
+  // only allow a valid UUID to be resolved for GET requests
+  if (validator.isUUID(identifier) && req.method.toUpperCase() === 'GET') {
+    res.status('200').send(`{"data":{"identifier":"${identifier}"}}}`)
+  }
+
+  // return not found
+  res.status('404').send(`{"error":{"message":"Not found"}}`)
+}
+
+app.use('/:identifier', restRequest)
 
 /*
  * Create a new ApolloServer instance, serving the GraphQL schema
@@ -21,17 +44,6 @@ const server = new ApolloServer({
   }
 })
 
-const app = express()
-
-const restRequest = function (req, res, next) {
-  const identifier = req.params.identifier
-  if (validator.isUUID(identifier) && req.method.toUpperCase() === 'GET') {
-    res.status('200').send(`{"data":{"identifier":"${identifier}"}}}`)
-  }
-  res.status('404').send()
-}
-
-app.use('/:identifier', restRequest)
 server.applyMiddleware({ app, path: '/' })
 
 /*
