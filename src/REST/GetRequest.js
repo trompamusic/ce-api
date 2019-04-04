d85f64bf8d2d0cabe1cebd965d6fc82100b4df1c
Wim Klerkx <wim@videodock.com>
2019-04-04 16:14:41 +0200

    add query creation

--- src/REST/GetRequest.js
+++ src/REST/GetRequest.js
@@ -21,11 +21,64 @@ class GetRequest {
    */
   get find () {
     if (validator.isUUID(this.identifier)) {
+      const payload = this._retrievePayload(this._runQuery(this._getQuery()))
+
       this.result.status = '200'
+      this.result.data = payload
       this.result.data = `{"data":{"identifier":"${this.identifier}"}}}`
     }
 
     return this.result
+  }
+
+  _getQuery () {
+    return `MATCH (n) WHERE n.identifier = "${this.identifier}" RETURN n`
+  }
+
+  /**
+   * @param query
+   * @param queryType
+   * @param publishChannel
+   * @returns {Promise<{from, to} | never>}
+   */
+  _runQuery (query, queryType, publishChannel) {
+    info(`query: ${query}`)
+    let session = driver.session()
+    let promise = session.run(query)
+      .then(result => {
+        let rt = result.records.map(record => {
+          return retrievePayload(record.get('_payload'), queryType)
+        })
+        const returnValue = rt[0]
+        return returnValue
+      })
+      .catch(function (error) {
+        throw Error(error.toString())
+      })
+
+    return promise
+  }
+
+  /**
+   * @param payload
+   * @param payloadType
+   * @returns {*}
+   */
+  _retrievePayload (payload, payloadType) {
+    switch (payloadType) {
+      case 'add':
+      case 'remove':
+        return {
+          from: retrieveNodeData(payload.from),
+          to: retrieveNodeData(payload.to)
+        }
+      case 'RequestControlAction':
+        return payload.properties
+      case 'UpdateControlAction':
+        return payload
+      default:
+        warning('Unknown payloadType encountered')
+    }
   }
 }
 
