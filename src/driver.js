76add589f11709ef1893fed59eb97f3bf289f898
Christiaan Scheermeijer <christiaan@videodock.com>
2020-01-15 13:57:28 +0100

    feat(neo4j): disable lossless integers

--- src/driver.js
+++ src/driver.js
@@ -10,5 +10,6 @@ export const driver = neo4j.driver(
   neo4j.auth.basic(
     process.env.NEO4J_USER || 'neo4j',
     process.env.NEO4J_PASSWORD || 'letmein'
-  )
+  ),
+  { disableLosslessIntegers: true }
 )
