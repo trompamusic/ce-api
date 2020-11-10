7b88001ec82f627dd75322edcc5012e38ec100c2
Christiaan Scheermeijer <christiaan@videodock.com>
2020-11-10 10:57:31 +0100

    feat(project): update dc terms and rdf urls

--- src/routes/helpers/transformers.js
+++ src/routes/helpers/transformers.js
@@ -4,10 +4,10 @@ import { isDateTime } from 'neo4j-driver/lib/temporal-types'
 
 // Used scopes dict
 const scopedContexts = {
-  dc: 'http://purl.org/dc/elements/1.1/',
+  dc: 'http://purl.org/dc/terms/',
   prov: 'http://www.w3.org/ns/prov#',
   skos: 'http://www.w3.org/2004/02/skos/core#',
-  rdf: 'https://www.w3.org/2000/01/rdf-schema'
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
 }
 
 /**
