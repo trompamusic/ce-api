b2ba53f39ebff20a726474efe402df2adaafdf13
Christiaan Scheermeijer <christiaan@videodock.com>
2021-03-08 11:35:41 +0100

    fix(project): make migrations backwards compatible
    The migration failed on a Neo4j 3.x database

--- src/migrations/01-constraints.js
+++ src/migrations/01-constraints.js
@@ -7,10 +7,21 @@ import { schema } from '../schema'
  * @returns {Promise<void>}
  */
 export default async (transactions, driver) => {
+  const result = await driver.session().run('call dbms.components() yield versions unwind versions as version RETURN version')
+
+  const version = result.records[0].get('version')
+  const major = parseInt(version)
+
   // remove the fulltext index, if this exists while calling assertSchema, we get an error
-  await driver.session().run(`CALL db.indexes() YIELD name, provider WHERE provider = "fulltext-1.0" 
+  if (major >= 4) {
+    await driver.session().run(`CALL db.indexes() YIELD name, provider WHERE provider = "fulltext-1.0" 
     CALL db.index.fulltext.drop(name)
     RETURN TRUE`)
+  } else {
+    await driver.session().run(`CALL db.indexes() YIELD indexName, provider WHERE indexName = "metadataSearchFields" 
+    CALL db.index.fulltext.drop(indexName)
+    RETURN TRUE`)
+  }
 
   // run the assertSchema which creates all constraints based on the @id directives
   await assertSchema({ schema, driver, debug: false, dropExisting: true })
