bb01ef300d01de0aa68650211936759aa49c7688
Christiaan Scheermeijer <christiaan@videodock.com>
2021-05-03 08:35:36 +0200

    chore(project): fix allowed methods order
    Fixes JWT endpoint not working anymore

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,20 +25,46 @@ const allowedMethods = (allowedMethods) => (req, res, next) => {
 }
 
 router.all('/', allowedMethods(['OPTIONS', 'POST']))
-router.all('/jwt', allowedMethods(['OPTIONS', 'POST']))
-router.all('/health', allowedMethods(['OPTIONS', 'GET']))
-router.all('/:identifier', allowedMethods(['OPTIONS', 'GET']))
 
 /**
  * Health check endpoint
  */
+router.all('/health', allowedMethods(['OPTIONS', 'GET']))
 router.get('/health', (req, res) => {
   res.status(200).send({ message: 'OK' })
+})
+
+router.all('/jwt', allowedMethods(['OPTIONS', 'POST']))
+router.post('/jwt', async (req, res) => {
+  const { id, apiKey, scopes } = req.body
+
+  if (!id || !apiKey) {
+    return res.status(401).send({
+      success: false,
+      message: 'The `apiKey` or `id` property is missing in the request body'
+    })
+  }
+
+  if (!scopes || !Array.isArray(scopes) || !scopes.length) {
+    return res.status(401).send({
+      success: false,
+      message: 'The `scopes` property is missing or not an Array with at least one scope'
+    })
+  }
+
+  try {
+    const jwt = generateToken(id, apiKey, scopes)
+
+    res.send({ success: true, jwt })
+  } catch (error) {
+    return res.status(403).send({ success: false, message: error.message })
+  }
 })
 
 /**
  * Get document by identifier
  */
+router.all('/:identifier', allowedMethods(['OPTIONS', 'GET']))
 router.get('/:identifier', (req, res) => {
   const { identifier } = req.params
   const accept = req.headers.accept || 'application/json'
@@ -80,32 +106,6 @@ router.get('/:identifier', (req, res) => {
 
       res.status(statusCode).send(errorString)
     })
-})
-
-router.post('/jwt', async (req, res) => {
-  const { id, apiKey, scopes } = req.body
-
-  if (!id || !apiKey) {
-    return res.status(401).send({
-      success: false,
-      message: 'The `apiKey` or `id` property is missing in the request body'
-    })
-  }
-
-  if (!scopes || !Array.isArray(scopes) || !scopes.length) {
-    return res.status(401).send({
-      success: false,
-      message: 'The `scopes` property is missing or not an Array with at least one scope'
-    })
-  }
-
-  try {
-    const jwt = generateToken(id, apiKey, scopes)
-
-    res.send({ success: true, jwt })
-  } catch (error) {
-    return res.status(403).send({ success: false, message: error.message })
-  }
 })
 
 export default router
